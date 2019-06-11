546c1f43ac046f58b09e02701220b7a147be4c27
Frick Nedrickson <fredricksonnicolas@gmail.com>
2019-06-10 17:39:34 -0700

    Revert "On second thought, don't pickle World Data"
    This reverts commit cc066462957167d815545a0de1d8961bb727a3a6.
    
    I was a fool. An accursed fool! You really do need to recreate the
    numpy array every time you pass a pickle boundry. Otherwise you're
    just churning CPU cycles.
    
    I think this fact was obscured from me by the __pycache__. I think.
    All I know is that it wasn't working for a new feature.

--- src/city/world/maker.py
+++ src/city/world/maker.py
@@ -29,7 +29,7 @@ def build_world_basic(world_data, complete_val, primary_ts, detail_ts):
         "tile_set" : primary_ts
     }
     perform_spatial_work(
-        paint.base.only_grass, world_data, 
+        paint.base.only_grass, world_data.get_picklable(), 
         kw_args=kw_args
     )
 
@@ -37,7 +37,7 @@ def build_world_basic(world_data, complete_val, primary_ts, detail_ts):
     lakes = paint.lake.generate_chain_lakes( world_data )
     kw_args = { "tile_set" : primary_ts }
     perform_work(
-        paint.lake.paint_square_lake_chain, world_data,
+        paint.lake.paint_square_lake_chain, world_data.get_picklable(),
         lakes, kw_args=kw_args
     )
 
@@ -64,7 +64,7 @@ def build_world_basic(world_data, complete_val, primary_ts, detail_ts):
         "world_ts" : primary_ts, "detail_ts" : detail_ts, 
     }
     perform_spatial_work(
-        paint.edge.edge_pass, world_data, 
+        paint.edge.edge_pass, world_data.get_picklable(), 
         kw_args=kw_args
     )
 
