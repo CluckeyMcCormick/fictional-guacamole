eafbe6cdc654a511f5d6ded061501b6176d3618a
Frick Nedrickson <fredricksonnicolas@gmail.com>
2019-06-23 22:33:30 -0700

    Add in a turret with some basic controls (Godot)
    To catch up with our Python prototype, I've added in a turret that
    rotates with the Q and E keys. This puts us on par with the
    development of our Prototype II Python development.
    
    That I was able to duplicate my Python development so quickly is a
    testament to Godot. However, I am struggling with the engine's system
    of nodes.
    
    In Pyglet, and cocos2d, a Sprite would be responsible for detecting
    collisions, physics and the like. Here, that isn't so - a Sprite is
    only for rendering. To add collisions, you need an Area2D node. To
    check it's visibility, you need a VisibilityNotifier node. To add
    physics, you need a KinematicBody node, and so forth.
    
    This more mix-and-match instances wouldn't be too bad but I'm my usual
    techniques (googling for examples and guides) just doesn't work, which
    is slowing my progress. Maybe I should start picking apart some
    templates...

---
+++ godot/scenes/Main.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    # The amount we'll rotate in either direction, in radians
+    var MAGNITUDE = PI / 2
+    var CLAMP_MIN = 0
+    var CLAMP_MAX = PI
+    # Our end rotation, in radians
+    var rot = 0 # The player's movement vector.
+    
+    if Input.is_action_pressed("tr_rotate_clockwise"):
+        rot += MAGNITUDE
+    if Input.is_action_pressed("tr_rotate_counterwise"):
+        rot -= MAGNITUDE
+    rot *= delta
+    
+    if rot != 0:
+        # See if we go under/over our min/max rotation
+        var clamp_rot = clamp( $Turret.rotation + rot, CLAMP_MIN, CLAMP_MAX )
+        # If the difference between the current rotation isn't the same...
+        if clamp_rot - $Turret.rotation != rot:
+            # Then we can only assume that we got clamped! Use the lesser
+            # rotational value.
+            rot = clamp_rot - $Turret.rotation
+        $Turret.rotate(rot)
+
