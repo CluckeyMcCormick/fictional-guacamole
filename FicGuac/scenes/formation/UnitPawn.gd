889b7a30357d427f9818a6abbba05b325333cd95
frick-nedrickson <CluckeyMcCormick@github.com>
2020-05-21 20:50:48 -0700

    Fix the UnitPawn physics freak out
    Previously, our UnitPawn object was animated using a RigidBody object.
    I was using this based on a hazy memory that this was appropriate for
    NPCs. As it turns out, that's for 2D. Or it's a lie. Either way,
    that's just not true in 3D.
    
    Trying to animate (i.e. move) a 3D RigidBody is a bit stupid. The body
    falls over, spins, and generally freaks out as you keep forcing the
    position and making the physics engine cry. It cries small tears, but
    cries all the same.
    
    The solution was to change UnitPawn to a 3D KinematicBody.
    KinematicBodies have more low-key physics simulations - just some
    basic collisions. It doesn't even have built-in gravity.
    
    Even after implementing the KinematicBody, I noticed that the UnitPawn
    freaked out a bunch. This was because it was trying to hew too closely
    to the follow-path and kept over-shooting left and right. Rapidly.
    Adding some give (in the form of PATHING_TOLERANCE) fixed this issue.
    
    While I was making PATHING_TOLERANCE a constant, it only made sense to
    constant-ize the tolerance we had for our goal point. And behold - the
    GOAL_TOLERANCE constant has been made!

--- FicGuac/scenes/formation/UnitPawn.gd
+++ FicGuac/scenes/formation/UnitPawn.gd
@@ -1,9 +1,15 @@
-extends RigidBody
+extends KinematicBody
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+# What's our tolerance for meeting our goal
+const GOAL_TOLERANCE = .1
+# What's our tolerance for straying/overshooting on our path to the goal
+const PATHING_TOLERANCE = .05
+
+# How fast do we move? (units/second)
 const MOVE_RATE = 1
 var move_order = null #Vector3(2, 2, 2)
 
@@ -11,24 +17,29 @@ var move_order = null #Vector3(2, 2, 2)
 func _ready():
     pass # Replace with function body.
 
-func _integrate_forces(body_state):
+func _physics_process(body_state):   
+    var dirs = Vector3.ZERO
     
-    if move_order == null:
-        return
+    if not self.is_on_floor():
+        dirs.y = -4.9
     
-    # Calculate the distance from our current global position
-    var dirs = move_order - self.global_transform.origin
-    # Use that distance to calculate a direction on each axis - do we need to go
-    # positively or negatively?
-    if dirs.x != 0:
-        dirs.x = dirs.x / abs(dirs.x)
-    if dirs.z != 0:
-        dirs.z = dirs.z / abs(dirs.z)
-    # Currently, we don't mess with y
-    dirs.y = 0
+    if move_order and self.global_transform.origin.distance_to(move_order) < GOAL_TOLERANCE:
+        move_order = null
     
-    self.set_linear_velocity( dirs * MOVE_RATE )
-    pass
+    if move_order != null:
+        # Calculate the distance from our current global position
+        dirs = move_order - self.global_transform.origin
+        # Use that distance to calculate a direction on each axis - do we need to go
+        # positively or negatively?
+        if dirs.x != 0 and abs(dirs.x) > PATHING_TOLERANCE:
+            dirs.x = dirs.x / abs(dirs.x)
+        if dirs.z != 0 and abs(dirs.z) > PATHING_TOLERANCE:
+            dirs.z = dirs.z / abs(dirs.z)
+ 
+    # If we're trying to move somewhere...
+    if dirs != Vector3.ZERO:
+        # Then move (with some snap)
+        self.move_and_slide_with_snap(dirs * MOVE_RATE, Vector3(0, 1, 0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
