f0fadbb156d244f3b6de2a8a081bc6c06e5648ae
frick-nedrickson <CluckeyMcCormick@github.com>
2020-07-11 16:06:15 -0700

    Update the Trees while still in the editor
    So, our tree sprites change to match the tree type and the light
    level. While this worked in game, the editor showed the trees as being
    the default Light Elm. This made it hard to guess exactly what the
    scene would look like until we ran the project.
    
    Using the "tool" keyword and some setters (which I didn't know
    existed!), the Tree scene will now update it's sprite AND collision
    mode - IN THE EDITOR - when the user sets the export values. Neat!
    
    Also, while I was at it, I updated the tree's collision masks. They
    had been marked as "terrain" when they were supposed to be marked as
    "scenery (blocking)".

--- FicGuac/scenes/formation/terrain/Tree.gd
+++ FicGuac/scenes/formation/terrain/Tree.gd
@@ -1,9 +1,13 @@
+# Set this as a tool so that we see the trees change in the editor
+tool
 extends StaticBody
 
+# What type of tree is this?
 enum TREE_TYPE {
     elm, pine, oak
 }
 
+# What's the light level of this tree?
 enum LIGHT_LEVEL {
     light, dark
 }
@@ -12,39 +16,57 @@ enum LIGHT_LEVEL {
 # var a = 2
 # var b = "text"
 
-export(TREE_TYPE) var _tree_type = TREE_TYPE.elm
-export(LIGHT_LEVEL) var _light_level = LIGHT_LEVEL.light
+export(TREE_TYPE) var _tree_type = TREE_TYPE.elm setget set_tree_type
+export(LIGHT_LEVEL) var _light_level = LIGHT_LEVEL.light setget set_light_level
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     # Immediately set the tree type using our preset tree type and light level
-    set_tree_type(self._tree_type, self._light_level)
+    _tree_refresh()
 
-func set_tree_type(new_tree_type, new_light_level):
-    # Disable all the collision models - we'll give ourselves a clean slate.
+func _tree_refresh():
+    # Disable and hide all the collision models - we'll give ourselves a clean
+    # slate.
+    # ELM
     $CollisionElm.disabled = true
+    $CollisionElm.visible = false
+    # PINE
     $CollisionPine.disabled = true
+    $CollisionPine.visible = false
+    # OAK
     $CollisionOak.disabled = true
+    $CollisionOak.visible = false
     
     var sprite_string = ""
     
     # Get the appropriate light level for our animation
-    match new_light_level:
+    match _light_level:
         LIGHT_LEVEL.light:
             sprite_string = "_light"
         LIGHT_LEVEL.dark:
             sprite_string = "_dark"
     
     # Set the collision status and get
-    match new_tree_type:
+    match _tree_type:
         TREE_TYPE.elm:
             $CollisionElm.disabled = false
+            $CollisionElm.visible = true
             sprite_string = "elm" + sprite_string
         TREE_TYPE.pine:
             $CollisionPine.disabled = false
+            $CollisionPine.visible = true
             sprite_string = "pine" + sprite_string
         TREE_TYPE.oak:
             $CollisionOak.disabled = false
+            $CollisionOak.visible = true
             sprite_string = "oak" + sprite_string
     
     $Sprite.animation = sprite_string
+
+func set_tree_type(new_tree_type):
+    _tree_type = new_tree_type
+    _tree_refresh()
+
+func set_light_level(new_light_level):
+    _light_level = new_light_level
+    _tree_refresh()
