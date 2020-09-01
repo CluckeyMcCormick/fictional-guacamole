2fbd3963fda13b7550bee0251392e6693c9cb48b
frick-nedrickson <CluckeyMcCormick@github.com>
2020-08-31 18:55:58 -0700

    Add columns
    We've created procedual walls, but the walls in this game aren't meant
    to meet. They were specifically designed with the intention that there
    would be a different component that bridge the texture gap between
    multiple walls.
    
    This commit adds that component - the column! The column is a square
    (since the walls should all be of the same thickness) and has the same
    texture on all four sides, as well as a top texture.
    
    I added some columns to our work-in-progress buildings along with some
    different textures. I think I might play around with the columns in
    the future - it might be worth it to have opposite sides with opposite
    materials. For now, though, I'm more than happy!

---
+++ FicGuac/scenes/formation/terrain/buildings/RothComponents/RothColumn.gd
@@ -0,0 +1,153 @@
+tool
+extends StaticBody
+
+export(Material) var top_mat setget set_top
+export(Material) var sides_mat setget set_sides
+
+# How long are the sides of the column?
+export(float, .25, 10, .25) var side_length = 1 setget set_length
+# How tall is this column?
+export(float, .25, 10, .25) var height = 2 setget set_height
+
+# Load the PolyGen script
+const PolyGen = preload("res://scenes/formation/util/PolyGen.gd")
+
+func _ready():
+    # When we enter the scene for the first time, we have to build out the
+    # stairs
+    self.build_all()
+
+# --------------------------------------------------------
+#
+# Setters and Getters
+#
+# --------------------------------------------------------
+func set_top(new_mat):
+    top_mat = new_mat
+    if Engine.editor_hint:
+        build_all()
+
+func set_sides(new_mat):
+    sides_mat = new_mat
+    if Engine.editor_hint:
+        build_all()
+
+func set_length(new_length):
+    side_length = new_length
+    if Engine.editor_hint:
+        build_all()
+
+func set_height(new_height):
+    height = new_height
+    if Engine.editor_hint:
+        build_all()
+
+# --------------------------------------------------------
+#
+# Build Functions
+#
+# --------------------------------------------------------
+func build_all():
+    build_top()
+    build_sides()
+    adjust_base_collision()
+
+func build_sides():    
+    var new_mesh = Mesh.new()
+    var verts = PoolVector3Array()
+    var UVs = PoolVector2Array()
+    
+    # Since the column is a square (top-down, at least), all the points are the
+    # same absolute value. We can just precalculate and vary the sign!
+    var abs_point = side_length / 2.0
+    
+    # Points and such
+    var pd
+    var pointA
+    var pointB 
+    
+    # Face 1: X-Positive
+    pointA = Vector2(-abs_point, 0)
+    pointB = Vector2( abs_point, height)
+    pd = PolyGen.create_xlock_face_simple(pointA, pointB, abs_point)
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+
+    # Face 2: Z-Negative
+    pointA = Vector2(-abs_point, 0)
+    pointB = Vector2( abs_point, height)
+    pd = PolyGen.create_zlock_face_simple(pointA, pointB, -abs_point)
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+
+    # Face 3: X-Negative
+    pointA = Vector2( abs_point, 0)
+    pointB = Vector2(-abs_point,  height)
+    pd = PolyGen.create_xlock_face_simple(pointA, pointB, -abs_point)
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+    
+    # Face 4: Z-Positive
+    pointA = Vector2( abs_point, 0)
+    pointB = Vector2(-abs_point, height)
+    pd = PolyGen.create_zlock_face_simple(pointA, pointB, abs_point)
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+    
+    var st = SurfaceTool.new()
+    st.begin(Mesh.PRIMITIVE_TRIANGLES)
+    st.set_material(sides_mat)
+
+    for v in verts.size():
+        st.add_uv(UVs[v])
+        st.add_vertex(verts[v])
+
+    st.generate_normals()
+    st.generate_tangents()
+
+    st.commit(new_mesh)
+    $Sides.mesh = new_mesh
+
+func build_top():
+    var new_mesh = Mesh.new()
+    var verts = PoolVector3Array()
+    var UVs = PoolVector2Array()
+
+    # Calculate the height (y-lock)
+    var y_lock = height / 2.0
+    # Since the column is a square (top-down, at least), all the points are the
+    # same absolute value. We can just precalculate and vary the sign!
+    var abs_point = side_length / 2.0
+    
+    # Create our points
+    var pointA = Vector2(-abs_point, -abs_point)
+    var pointB = Vector2( abs_point,  abs_point)
+    
+    # Make the upward Y-face
+    var pd = PolyGen.create_ylock_face_simple(pointA, pointB, height)
+    verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+    UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+    
+    var st = SurfaceTool.new()
+    st.begin(Mesh.PRIMITIVE_TRIANGLES)
+    st.set_material(top_mat)
+
+    for v in verts.size():
+        st.add_uv(UVs[v])
+        st.add_vertex(verts[v])
+
+    st.generate_normals()
+    st.generate_tangents()
+
+    st.commit(new_mesh)
+    $Top.mesh = new_mesh
+
+func adjust_base_collision():
+    # Calculate the new size for the collision box
+    var new_size = Vector3(side_length / 2.0, height / 2.0, side_length / 2.0)
+    
+    # Reset the foundation back to zero so we know what we're doing
+    $Collision.transform.origin = Vector3.ZERO
+    $Collision.shape = BoxShape.new()
+    $Collision.shape.extents = new_size
+    $Collision.translate(Vector3(0, height / 2.0, 0))
