f4569c7367c146750e72b43b0bcd12345839e9e9
frick-nedrickson <CluckeyMcCormick@github.com>
2020-08-31 22:44:49 -0700

    Update the stairs
    There were a couple of things about the "stairs" procedural component
    that bugged me.
    
    First, the "Sides" of the stairs were grouped by material and mesh to
    both of the z-parallel faces, but also the x-parallel face. Part of
    the reason I've avoided texturing the stairs is because I couldn't
    think of a texture the would look good on both faces.
    
    The solution, then, was to make the x-parallel face it's own mesh as
    well as material. This should help make our stairs easier to manage,
    though I might have to mess with the textures later on.
    
    Secondly, I made the decision to have the stairs not extend to their
    full height for some reason. Honestly I can't remember why. I think
    that I thought it was disingeous to have a step match up with the
    foundations when that step wasn't real.
    
    But it looked ugly, and made texturing more difficult. So I've added a
    toggle to go between the "full top step" stairs and the previous style
    of stairs. Just in case I want those old stairs.

--- FicGuac/scenes/formation/terrain/buildings/RothComponents/RothStairs.gd
+++ FicGuac/scenes/formation/terrain/buildings/RothComponents/RothStairs.gd
@@ -2,8 +2,11 @@ tool
 extends StaticBody
 
 # Required materials
+export(Material) var forwards_mat
 export(Material) var sides_mat
 export(Material) var tops_mat
+
+export(bool) var full_top_step = true setget set_full_top
 
 export(float, 1, 10, .25) var x_length = 2 setget set_x_length
 export(float, 1, 10, .25) var z_length = 2 setget set_z_length
@@ -53,6 +56,11 @@ func set_target_height(new_target):
     if Engine.editor_hint:
         build_all()
 
+func set_full_top(new_val):
+    full_top_step = new_val
+    if Engine.editor_hint:
+        build_all()
+
 # --------------------------------------------------------
 #
 # Build Functions
@@ -60,15 +68,25 @@ func set_target_height(new_target):
 # --------------------------------------------------------
 func build_all():
     build_tops()
-    build_collision()
     build_sides()
+    build_forwards()
+    build_collision()
 
 func build_tops():
+    # How tall is each individual step? In other words, how much taller is each
+    # step over the step that preceeded it?
+    var height_step
     
-    # For the purpose of calculating how high each step should be, we consider
-    # there to be 1 extra step, since we stop just BEFORE the height of the
-    # foundation
-    var height_step = self.target_height / float(self.steps + 1)
+    if full_top_step:
+        # If we have a top step AT level with our height, then the height per
+        # step is just the height divided by the number of steps. Easy!
+        height_step = self.target_height / float(self.steps)
+    else:
+        # If we don't have a "full top step", then the steps need to stop short
+        # of the foundation (rising up the foundation would be the "full top
+        # step"). So, act as though there is 1 extra step, since we stop just 
+        # BEFORE the height of the foundation
+        height_step = self.target_height / float(self.steps + 1)
     
     # We want the specified number of steps on Z, though
     var z_step = self.z_length / float(self.steps)
@@ -120,14 +138,23 @@ func build_tops():
     st.generate_tangents()
 
     st.commit(new_mesh)
-    $StairTops.mesh = new_mesh
+    $Tops.mesh = new_mesh
 
 func build_sides():
+    # How tall is each individual step? In other words, how much taller is each
+    # step over the step that preceeded it?
+    var height_step
     
-    # For the purpose of calculating how high each step should be, we consider
-    # there to be 1 extra step, since we stop just BEFORE the height of the
-    # foundation
-    var height_step = self.target_height / float(self.steps + 1)
+    if full_top_step:
+        # If we have a top step AT level with our height, then the height per
+        # step is just the height divided by the number of steps. Easy!
+        height_step = self.target_height / float(self.steps)
+    else:
+        # If we don't have a "full top step", then the steps need to stop short
+        # of the foundation (rising up the foundation would be the "full top
+        # step"). So, act as though there is 1 extra step, since we stop just 
+        # BEFORE the height of the foundation
+        height_step = self.target_height / float(self.steps + 1)
     
     # We want the specified number of steps on Z, though
     var z_step = self.z_length / float(self.steps)
@@ -169,15 +196,8 @@ func build_sides():
         )
         verts.append_array( pd[PolyGen.VECTOR3_KEY] )
         UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
-        
-        # Face 2
-        pd = PolyGen.create_zlock_face_simple(
-            Vector2(right_x, base_height), Vector2(left_x, top_height), far_z
-        )
-        verts.append_array( pd[PolyGen.VECTOR3_KEY] )
-        UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
 
-        # Face 3
+        # Face 2
         pd = PolyGen.create_xlock_face_simple(
             Vector2(z_base, base_height), Vector2(far_z, top_height), right_x
         )
@@ -196,10 +216,80 @@ func build_sides():
     st.generate_tangents()
 
     st.commit(new_mesh)
-    $StairSides.mesh = new_mesh
+    $Sides.mesh = new_mesh
+
+func build_forwards():
+    # How tall is each individual step? In other words, how much taller is each
+    # step over the step that preceeded it?
+    var height_step
+    
+    if full_top_step:
+        # If we have a top step AT level with our height, then the height per
+        # step is just the height divided by the number of steps. Easy!
+        height_step = self.target_height / float(self.steps)
+    else:
+        # If we don't have a "full top step", then the steps need to stop short
+        # of the foundation (rising up the foundation would be the "full top
+        # step"). So, act as though there is 1 extra step, since we stop just 
+        # BEFORE the height of the foundation
+        height_step = self.target_height / float(self.steps + 1)
+    
+    # We want the specified number of steps on Z, though
+    var z_step = self.z_length / float(self.steps)
+    
+    # The stairs are positioned at the front of the foundation, so capture the
+    # length of the foundation
+    var z_base = -self.z_length / 2.0
+    
+    # Pre-calculate our points on the left-and-right-hand side
+    var left_x = -self.x_length / 2.0
+    var right_x = self.x_length / 2.0
+    
+    var new_mesh = Mesh.new()
+    var verts = PoolVector3Array()
+    var UVs = PoolVector2Array()
+    
+    for i in range(self.steps):
+        # Calculate the upper-step height. Calculate with "current step" as the
+        # total steps minus the current count; this has the effect of counting 
+        # downard as the steps go on - like stairs!
+        var top_height = float(self.steps - i) * float(height_step)
+        # We need to do the same thing to caluclate the height, but for the step
+        # ahead of us
+        var base_height = float(self.steps - (i + 1)) * float(height_step)
+        # Calculate the z length of this step
+        var z_len = float(i + 1) * z_step
+        # Calculate the z of our step that's farther from the origin
+        var far_z = z_base + z_len
+        
+        # If we're on the final step, automatically set the far z value to the
+        # full z length. This will ensure our stair steps fit FULLY into the
+        # appropriate length. 
+        if i == (self.steps - 1):
+            far_z = z_base + self.z_length
+        
+        # Face 1
+        var pd = PolyGen.create_zlock_face_simple(
+            Vector2(right_x, base_height), Vector2(left_x, top_height), far_z
+        )
+        verts.append_array( pd[PolyGen.VECTOR3_KEY] )
+        UVs.append_array( pd[PolyGen.VECTOR2_KEY] )
+    
+    var st = SurfaceTool.new()
+    st.begin(Mesh.PRIMITIVE_TRIANGLES)
+    st.set_material(forwards_mat)
+
+    for v in verts.size():
+        st.add_uv(UVs[v])
+        st.add_vertex(verts[v])
+
+    st.generate_normals()
+    st.generate_tangents()
+
+    st.commit(new_mesh)
+    $Forwards.mesh = new_mesh
 
 func build_collision():
-    
     # The stairs are positioned at the front of the foundation, so capture the
     # length of the foundation
     var z_base = -self.z_length / 2.0
@@ -224,6 +314,4 @@ func build_collision():
     verts.push_back( Vector3(right_x, height, z_base) )
     
     new_shape.set_points(verts)
-    $StairCollision.shape = new_shape
-
-
+    $Collision.shape = new_shape
