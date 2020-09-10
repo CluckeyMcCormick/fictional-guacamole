7ecdf12fdab5e17d851601b51bd74d222a74aa01
frick-nedrickson <CluckeyMcCormick@github.com>
2020-09-09 21:41:44 -0700

    Redo the trees in Fantasy24 pallette
    I redid the crops and the grass; the next step was redoing the trees
    in the Fantasy24 pallette.
    
    This illustrated several issues to me. Firstly - we need to make trees
    of our own. These current trees are old ones by SURT, and are too low
    res for our current standard (32 pixels to a wold unit).
    
    The other issue is the pallette. The problem with the original
    pallette - which I believe was the NES pallette - was that it lacked
    enough browns colors to appropriately show the wood-heavy setting
    appropriately. The problem now is that the current pallette utterly
    lacks the amount of greens required by NATURE, which is the game's
    setting. While the yellowish highlights look really good on the trees,
    they look pretty awful on the grass.

--- FicGuac/scenes/nature/Tree.gd
+++ FicGuac/scenes/nature/Tree.gd
@@ -7,17 +7,11 @@ enum TREE_TYPE {
     elm, pine, oak
 }
 
-# What's the light level of this tree?
-enum LIGHT_LEVEL {
-    light, dark
-}
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 export(TREE_TYPE) var _tree_type = TREE_TYPE.elm setget set_tree_type
-export(LIGHT_LEVEL) var _light_level = LIGHT_LEVEL.light setget set_light_level
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,40 +27,22 @@ func _tree_refresh():
     # PINE
     $CollisionPine.disabled = true
     $CollisionPine.visible = false
-    # OAK
-    $CollisionOak.disabled = true
-    $CollisionOak.visible = false
     
     var sprite_string = ""
-    
-    # Get the appropriate light level for our animation
-    match _light_level:
-        LIGHT_LEVEL.light:
-            sprite_string = "_light"
-        LIGHT_LEVEL.dark:
-            sprite_string = "_dark"
     
     # Set the collision status and get
     match _tree_type:
         TREE_TYPE.elm:
             $CollisionElm.disabled = false
             $CollisionElm.visible = true
-            sprite_string = "elm" + sprite_string
+            sprite_string = "elm"
         TREE_TYPE.pine:
             $CollisionPine.disabled = false
             $CollisionPine.visible = true
-            sprite_string = "pine" + sprite_string
-        TREE_TYPE.oak:
-            $CollisionOak.disabled = false
-            $CollisionOak.visible = true
-            sprite_string = "oak" + sprite_string
-    
+            sprite_string = "pine"
+            
     $Sprite.animation = sprite_string
 
 func set_tree_type(new_tree_type):
     _tree_type = new_tree_type
-    _tree_refresh()
-
-func set_light_level(new_light_level):
-    _light_level = new_light_level
     _tree_refresh()
