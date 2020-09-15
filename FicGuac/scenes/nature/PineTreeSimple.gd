f8aa866d9f0c17c862d779cd13b3a9185281737a
frick-nedrickson <CluckeyMcCormick@github.com>
2020-09-14 18:41:56 -0700

    Fix Woods-lag problem by using Quads
    After my last commit, I couldn't stop thinking about how, just maybe,
    I could fix the woods by swapping over to using quads instead of
    Sprites. So I did and - you know what? IT WORKED!
    
    The game runs beautifully when you're just using a crapton of quads.
    Like I couldn't even believe how well it was running! And that was
    with shadows enabled on the Quads! I haven't bothered testing
    collision yet, but that'll come later. Right now we're gonna take the
    quad victory and run with it.

--- FicGuac/scenes/nature/PineTreeSimple.gd
+++ FicGuac/scenes/nature/PineTreeSimple.gd
@@ -7,10 +7,14 @@ enum TREE_TYPE {
     pine_a, pine_b, pine_c
 }
 
+# Preload our selections for the (T)ree (M)aterials
+const TM_PINE_A = preload("res://assets/nature/trees/mat_ztree01_fc_a.tres")
+const TM_PINE_B = preload("res://assets/nature/trees/mat_ztree01_fc_b.tres")
+const TM_PINE_C = preload("res://assets/nature/trees/mat_ztree01_fc_c.tres")
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 export(TREE_TYPE) var _tree_type = TREE_TYPE.pine_a setget set_tree_type
 
 # Called when the node enters the scene tree for the first time.
@@ -19,26 +23,27 @@ func _ready():
     _tree_refresh()
 
 func _tree_refresh():
-    var sprite_string = ""
+    var material = null
     
-    # Set the collision status and get
+    if not get_node("Mesh"):
+        return
+    
     match _tree_type:
         TREE_TYPE.pine_a:
-            sprite_string = "pine_fc_a"
+            material = TM_PINE_A
         TREE_TYPE.pine_b:
-            sprite_string = "pine_fc_b"
+            material = TM_PINE_B
         TREE_TYPE.pine_c:
-            sprite_string = "pine_fc_c"
-            
-    $Sprite.animation = sprite_string
+            material = TM_PINE_C
+    
+    $Mesh.set_surface_material(0, material)
 
 func set_tree_type(new_tree_type):
     _tree_type = new_tree_type
     _tree_refresh()
 
-
 func _on_VisibilityNotifier_screen_entered():
-    $Sprite.visible = true
+    $Mesh.visible = true
 
 func _on_VisibilityNotifier_screen_exited():
-    $Sprite.visible = false
+    $Mesh.visible = false
