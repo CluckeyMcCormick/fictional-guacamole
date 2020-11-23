8de6ee825ace96f2f7ca014e5c28a24d8fc9014a
frick-nedrickson <CluckeyMcCormick@github.com>
2020-11-22 23:23:12 -0800

    Create clearer silhouettes using ImageMagick scripts
    As I outlined in the last commit, I was having an issue with our
    silhouettes. The feathered edges were making an exact outline
    difficult. It was hard to find just the right value to cut away the
    silhouette at without cutting into the body of the sprite.
    
    I wanted to see if I could fix this in Blender. Maybe it's possible. I
    gave up, because the workflow of Blender is anathema to my programmer
    brain.
    
    Instead, I decided to use an image-processing program, ImageMagick, to
    handle the processing for my sprites. Since ImageMagick is a command
    line utility, this allows us to write scripts for easy and repeatable
    batch processing. So, I've created two scripts: a processing script
    and a spritesheet script. The processing script drops the feathered
    edges from the input sprites and replaces them with a solid black
    outline. The spritesheet just packs the arguments into a spritesheet
    (we haven't had dedicated utility for that, so this is nice).
    
    Using our new scripts, I created a new pawn spritesheet. I made them
    a neutral color because I thought it would go better with the shader,
    though the more I think about it the less I think that actually
    matters.
    
    Now I'm interested to see if I can do more post-processing to get
    better edge-highlighting on the sprites. I also want to see about
    posterizing and possible dithering. I should also document the scripts
    and what they do, including alternatives explaining the basic
    techniques. And finally, I really want to change the silhouette shader
    so that the alpha of our input color actually gets used.

---
+++ scripts/ImageMagick/sprite_processing.bash
@@ -0,0 +1,87 @@
+
+# ~~~~~~~~~~~~~~~~~~~~
+# Step 0: Script Setup
+# ~~~~~~~~~~~~~~~~~~~~
+
+# Bash-specific script check - is the first argument we got passed a directory?
+if [ -d $1 ]; then
+    echo "Valid directory identified!"
+else
+    exit
+fi
+
+# Change directory to the working directory - that's the only place we'll be
+# working!
+cd $1
+
+# Bash-specific script check - does the second argument exist?
+if [ -z "$2" ]; then
+    echo Wildcard not provided! Please provide a wildcard to process.
+    exit
+fi
+
+# Bash-specific script check - does the second argument, when we're in the first
+# argument directory, actually give us anything?
+if compgen -G "$2" > /dev/null; then
+    echo Valid files identified!
+else
+    echo Nothing found for "$1/$2"!
+    exit
+fi
+
+# File (PR)e(F)ix - we create and destroy files as part of this script. To avoid
+# collisions with user files, we'll use this elongated prefix for files we make
+# and delete (except for the output!)
+prf=sprite_processing_staged_image
+
+# ~~~~~~~~~~~~~~~~~
+# Step 1: "Stage A"
+# ~~~~~~~~~~~~~~~~~
+
+# This command takes all the png images in a folder and does a "threshold" (of
+# 0) on the alpha channel. Since the threshold is 0, any value above is pushed
+# to 255/256, and any value at 0 stays at 0. This ensures we have no
+# partial-transperancy pixels.
+magick convert $2 -channel a -threshold 0 +channel $prf%04d_a.png
+
+# ~~~~~~~~~~~~~~~~~
+# Step 2: "Stage B"
+# ~~~~~~~~~~~~~~~~~
+# This command takes the output from above and runs it through an edge detection
+# algorithm. It then pushes the edge through a threshold to ensure the edge is
+# purely black. Output is just the edge pixels from the Stage A images.
+magick convert $prf*_a.png -edge 1 -channel RGB -threshold 100% $prf%04d_b.png
+
+# ~~~~~~~~~~~~~~~~~
+# Step 3: "Stage C"
+# ~~~~~~~~~~~~~~~~~
+# Now we have some simplified sprites and some sprite outlines, we need to
+# overlay those outlines on top of the sprites.
+# Get a list of all the files we made. Since they were organized in a very
+# particular way, this we'll get a list that alternates "a" file then "b" file.
+arr=(`ls $prf*.png`)
+# Count the number of entries in the in the array.
+img_count=${#arr[*]}
+# For every OTHER entry in the array...
+for ((i=0; i<img_count; i=i+2)); do
+    # The "a" entry is the current entry
+    j=$((i))
+    # The "b" entry is the next entry
+    k=$((i+1))
+    # File output count is i / 2
+    l=$((i/2))
+    # Pre-format the output file name (so we don't have to later)
+    out_file=`printf "output%04d_.png" ${l}`
+    # This command places the first file on top of the second file - in this
+    # case, that means placing the edge-only image on top of the
+    # anti-transperancy sprite. This gives us a new sprite with more clear
+    # edges.
+    magick composite -gravity center ${arr[$k]} ${arr[$j]} $out_file
+done
+
+# ~~~~~~~~~~~~~~~~~
+# Step 4: "Stage C"
+# ~~~~~~~~~~~~~~~~~
+# Remove our WIP images
+rm $prf*_a.png
+rm $prf*_b.png
---
+++ scripts/ImageMagick/sprite_sheetmaker.bash
@@ -0,0 +1,37 @@
+
+# ~~~~~~~~~~~~~~~~~~~~
+# Step 0: Script Setup
+# ~~~~~~~~~~~~~~~~~~~~
+
+# Bash-specific script check - is the first argument we got passed a directory?
+if [ -d $1 ]; then
+    echo Valid directory identified!
+else
+    echo "$1" is not a directory!
+    exit
+fi
+
+# Bash-specific script check - does the second argument, combined with the
+# first, actually give us anything?
+if compgen -G "$1/$2" > /dev/null; then
+    echo Valid files identified!
+else
+    echo Nothing found for "$1/$2"!
+    exit
+fi
+
+# Bash-specific script check - does the third argument actually match a
+# specification for a spritesheet matrix?
+if [[ $3 =~ [0-9]+x[0-9]+ ]]; then
+    echo Valid files size matrix identified!
+else
+    echo Image matrix \"$3\" has an invalid form!
+    echo Must match form CxR...
+    echo ----\> Where C is the number of columns \(i.e. frames per animation\)
+    echo ----\> and R is the number of rows \(i.e. different angles\)
+    exit
+fi
+
+# Composite together all of the images the user gave us into a singular
+# spritesheet. Neat!
+magick montage $1/$2 -tile $3 -geometry +0+0 -background none $1/spritesheet.png
