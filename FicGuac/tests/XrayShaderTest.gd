4a52d8323375933710f48e37fbaca605024d1082
frick-nedrickson <CluckeyMcCormick@github.com>
2020-11-27 13:20:06 -0800

    Test how X-Ray shaders interact with each other
    We've been manually switching between our different shaders to observe
    their effects. However, we've been unable to observe how shaders
    behave when interacting with each other in a near-game facsimile.
    
    So, I added a whole bunch of Pawns. I modified them to use a variety
    of sprites (to help tell them apart) and attached different shaders.
    This included two multiply shaders, two fill shaders, and an X-ray
    shader. The shaders were modified with various colors and alphas to
    help further differentiate the Pawns.
    
    To order the pawns around, I added a "Telepointer" and new processing.
    Whenever a pawn completes a path and asks for a new one, the
    telepointer moves randomly to assign a new path to the requesting
    pawn.
    
    Deciphering exactly what was happening was difficult because all the
    pawns moved so quickly. While that may be a problem in future builds,
    it's just a minor annoyance here. So I added a GUI slider to allow for
    fine control of the Engine speed. Easy slow motion.
    
    Running the actual test, here's what I observed - any two shader
    shapes were generally fine overlapping with each other out in the
    open. Whenever two shaders overlapped behind a building, whatever
    shader was considered to be in "front" rendered in front. That sounds
    obvious but considering that we purposefully disabled depth testing
    for the shader materials, I wasn't sure exactly what would happen.
    
    This has important implications with how the X-Ray shader interacts
    with the Silhouette shader. The X-Ray shader is meant to be a clear
    view through walls. However, if a Silhouette shader happens to be in
    front of an X-Ray shader, then the silhouette fills in over the
    appropriate spot in the X-Ray shader. This is particularly noticeable
    with the Fill shader, and is apparent, but less noticeable, with the
    Multiply shader. We'll need to see if it's possible to force the X-Ray
    to render in front.
    
    Also interesting - Silhouettes shaders will shade anything behind
    them. We just need to reduce the size of the material shader. All that
    being said, there's just a lot of problems we could solve with
    Godot 4.0, but will need work arounds for now. Who knows when that
    will happen...

--- FicGuac/tests/XrayShaderTest.gd
+++ FicGuac/tests/XrayShaderTest.gd
@@ -1,57 +1,62 @@
 extends Spatial
 
-# When the user clicks on the screen, we need to project a ray into the world
-# to determine what exactly got clicked - how long is that ray?
-const MOUSE_RAY_LENGTH = 1000
+var pawn_waiting_list = []
 
-var click_waiting = false
-var yielded_click
+func _ready():
+    # To start the ball rolling, we need to assign starting targets to any and
+    # all pawns in the scene.
+    pawn_waiting_list.append($BubblePawn)
+    pawn_waiting_list.append($MultiplyPawnA)
+    pawn_waiting_list.append($MultiplyPawnB)
+    pawn_waiting_list.append($FillPawnA)
+    pawn_waiting_list.append($FillPawnB)
+
+# Special clean up function - we have to reset the time scale when we exit.
+func _exit_tree():
+    Engine.set_time_scale(1)
 
 # Called every physics... frame? Cycle? Tick? Doesn't matter - 'delta' is the
 # elapsed time since the previous frame.
 func _physics_process(delta):
-    if click_waiting:
-        yielded_click.resume()
-
-# Process an input event. Intended for single-press input events (i.e. a button
-# push/click).
-func _input(event):
-    if event.is_action_pressed("formation_order"):
-        yielded_click = process_mouse_click()
-
-func process_mouse_click():
-    # Get the mouse position
-    var mouse_pos = get_viewport().get_mouse_position()
-    # Calculate the "from" vector (more of a point, really - the point our
-    # raycast will project FROM)
-    var mouse_from = $CoreCamera.project_ray_origin( mouse_pos )
-    # Calculate a point to raycast "to" by adding elongated normal
-    var mouse_to = $CoreCamera.project_ray_normal( mouse_pos ) * MOUSE_RAY_LENGTH
-    mouse_to += mouse_from
+    # While we still have pawns to process...
+    while not pawn_waiting_list.empty():
+        # We're processing the first pawn on the stack! Get it and remove it.
+        var pawn = pawn_waiting_list.pop_front()
+        # Is this location we just tested valid?
+        var valid_loc = false
     
-    # Yield until we can query. Make note that we are waiting on a click
-    click_waiting = true
-    yield()
-    
-    # Get our detour mesh
-    var detour_mesh = $DetourNavigation/DetourNavigationMesh
-    
-    # We're back! We should now be in the _physics_process, so we can query the
-    # world using raycasts. First, let's get the space state.
-    var space_state = get_world().direct_space_state
-    # QUERY, QUERY, QUERY!
-    var result = space_state.intersect_ray(
-        mouse_from, mouse_to, # Ray origin, Ray destination
-        [], # Node exclusion list; we exlude nothing!
-        1 # Collision MASK - what to collide with. BIT 0 is terrain (floor/path)
-    )
-    
-    # If our query actually got something...
-    if result:
-        # Get the path
-        var path = detour_mesh.find_path($Pawn.get_translation(), result.position)
+        # While we don't have a valid location to send the Pawn to...
+        while not valid_loc:
+            # First, choose a random position between -10 and 10 on x & z
+            var new_x = (randf() * 20) - 10
+            var new_z = (randf() * 20) - 10
+            
+            # Move the Telepointer there.
+            $Telepointer.translation.x = new_x
+            $Telepointer.translation.z = new_z
+            
+            # Force updates
+            $Telepointer/FloorCast.force_raycast_update()
+            $Telepointer/ObstacleCast.force_raycast_update()
+            
+            # This is a valid location IF the Floor Cast is colliding, but NOT
+            # the Obstacle Cast
+            valid_loc = $Telepointer/FloorCast.is_colliding()
+            valid_loc = valid_loc and not $Telepointer/ObstacleCast.is_colliding()
+            
+        # Telepointer is now at a valid location. Nice! Get a path from the
+        # current pawn to wherever we're colliding
+        var path = $DetourNavigation/DetourNavigationMesh.find_path(
+            pawn.get_translation(),
+            $Telepointer/FloorCast.get_collision_point()
+        )
         # Set the path!
-        $Pawn.current_path = Array(path["points"])
-        
-    # Either way, we're no longer waiting on a click
-    click_waiting = false
+        pawn.current_path = Array(path["points"])
+
+func _on_AnyPawn_path_complete(pawn, position):
+    # Stick this pawn on the stack
+    pawn_waiting_list.append(pawn)
+
+func _on_Slider_value_changed(value):
+    $GUI/SliderValue.text = str(value)
+    Engine.set_time_scale(value)
