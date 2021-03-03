310cd6e0f0023c79f95aa3344dc8f7ccac931f92
frick-nedrickson <CluckeyMcCormick@github.com>
2021-03-02 19:51:21 -0800

    Add a "wander randomly" task to carry that behavior forward.

---
+++ FicGuac/motion_ai/common/tasking/WanderRandom.gd
@@ -0,0 +1,42 @@
+extends "res://motion_ai/common/tasking/TaskTemplate.gd"
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Utility Functions
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~0
+
+func initialize(machine_root, physics_travel_region, wander_distance):
+    # Initialize the template's variables
+    _template_initialize(machine_root, physics_travel_region)
+    
+    # Start with a vector of fixed length
+    var point = Vector3(1, 0, 0)
+    
+    # Spin the vector to give us a random angle
+    point = point.rotated(Vector3.UP, deg2rad(randi() % 360))
+
+    # Now, normalize it and then scale the normalization by our move distance
+    # configurable
+    point = point.normalized() * wander_distance
+    
+    # Add it on to the integrating body's current position
+    point += target.global_transform.origin
+    
+    # Pass the position down to the states that need it.
+    $MoveToPosition._target_position = point
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Signal Processing Functions
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+func _on_MoveToPosition_action_failure(failure_code):
+    # We only had one action, so that's a fail I guess.
+    emit_signal("task_failed")
+    
+func _on_MoveToPosition_action_success():
+    # Our singular action suceeded, so the task suceeds
+    emit_signal("task_succeeded")
+
